7ba81e0b76deb70a01dd010524275a864384682b
phits <john@fitz.com>
2017-02-10 14:19:27 -0500

    Finished Section 5

--- base/js/script.js
+++ base/js/script.js
@@ -1,27 +1,75 @@
 $(function() {
 
+	// Retieving and Changing the Content of an Element
+	//  text(), html()
+	var firstPar = $("p:first");
+	console.log( firstPar.text() );
+	console.log( firstPar.html() );
+	console.log( $("p").html() );
+
+	// firstPar.text("<strong>Hello</strong> World");
+	// firstPar.html("<strong>Hello</strong> World");
+
+    firstPar.html( firstPar.html() + " This was just appended." );
+
+	// Changing Data of 
+	// var gallery = $(".gallery")
+ //    var images = [
+ //         "images/laptop-mobile_small.jpg",
+ //         "images/laptop-on-table_small.jpg",
+ //         "images/people-office-group-team_small.jpg"
+ //    ];
+
+ //    gallery.data("availableImages", images);
+ //    console.log(gallery.data("availableImages"));
+ //    gallery.data("name", "The Awesome Gallery");
+ //    console.log(gallery.data());
+ //    gallery.removeData("name");
+ //    console.log(gallery.data("name"));
+
+ //    var firstPar = $("p:first");
+ //    console.log(firstPar.data("mydata"));
+
+	// Add and Remove CSS Classes
+    // $("a").addClass("fancy-link");
+    // $("p:first").addClass("large emphasize");
+
+    // $("li li").addClass(function(index) {
+    // 	$(this).addClass("item-"+ index);
+    // });
+
+    // $("div").addClass(function(index, currentClass) {
+    // 	if(currentClass === "dummy") {
+    // 	    $(this).addClass("red-box");	
+    // 	}
+    // });
+
+    // $(".red-box").removeClass("red-box").addClass("blue-box");
+
+    // $(".dummy").removeClass("blue-box").addClass("green-box");
+
 	// Changing CSS Properties of Elements
-    $(".gallery").css("display", "none");
+    // var redBox = $(".red-box");
+    // console.log(redBox.css("width"));
+    // console.log(redBox.width());
 
-    var redBox = $(".red-box");
-    console.log(redBox.css("width"));
-    console.log(redBox.width());
-
-    redBox.css("background-color", "pink");
-    $('p').css("font-size", "18px");
-    redBox.css("width", "+=20px");
+    // redBox.css("background-color", "pink");
+    // $('p').css("font-size", "18px");
+    // redBox.css("width", "+=20px");
     
-    var properties = $("p").css(["font-size", "line-height", "color"])
-    console.log(properties);
-    console.log(properties["font-size"]);
+    // var properties = $("p").css(["font-size", "line-height", "color"])
+    // console.log(properties);
+    // console.log(properties["font-size"]);
 
-    redBox.css("user-select", "none");
+    // redBox.css("user-select", "none");
 
-    redBox.css("user-select", function() {
-    	return "none";
-    });
+    // redBox.css("user-select", function() {
+    // 	return "none";
+    // });
 
-	//Image Slide Show
+	// //Image Slide Show
+    // $(".gallery").css("display", "none");
+
     // var galleryImage = $(".gallery").find("img").first();
     // var images = [
     //      "images/laptop-mobile_small.jpg",
