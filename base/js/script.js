f9fc9eedac191bd9fa57e6e29a59ec1c5669b3af
phits <john@fitz.com>
2017-02-10 19:01:14 -0500

    Started Section 7

--- base/js/script.js
+++ base/js/script.js
@@ -1,8 +1,23 @@
 $(function() {
 
 	// The Focus & Blur Events
-	
+	var inputFields = $("input:text, input:password, textarea");
+	inputFields.focus(function() {
+		$(this).css("box-shadow", "0 0 4px #666")
+	});
 
+    inputFields.blur(function() {
+        $(this).css("box-shadow", "none")
+    });
+
+    $("#name").blur(function() {
+       var text = $(this).val();
+       if(text.length < 3) {
+           $(this).css("box-shadow", "0 0 8px #811");
+       } else {
+           $(this).css("box-shadow", "0 0 8px #181");
+       }
+    });
 
 	// Handling KeyDown & KeyUp Events
 	// keypress() - EVIl
